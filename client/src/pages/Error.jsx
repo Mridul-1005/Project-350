e00493619fa9e736e994ddd626b546cc8564051e
Mridul-1005 <mhdanger193@gmail.com>
2025-06-14 18:34:45 +0600

    Added error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
+
 
 const Error = () => {
+
     const error = useRouteError();
     console.log(error);
-  return <div>
-    <h1>Error Page</h1>
-    <Link to='/'>Back to home page</Link>
+    if(error.status === 404) {
+        return <Wrapper>
+            <div>
+                <img src={img} alt="not found"  />
+                <h3>Page not found</h3>
+                <p>We can't seem to find the page you're looking for.</p>
+                <Link to='/dashboard'>Back Home</Link>
+            </div>
+        </Wrapper>
+
+    }
+  return <Wrapper>
+  <div>
+    <h3>Something went wrong</h3>
   </div>
+  </Wrapper>
 }
 
 export default Error
