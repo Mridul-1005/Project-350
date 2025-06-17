f026106ab88f15091de7af3bd358eeb9647f2ff7
Mridul-1005 <mhdanger193@gmail.com>
2025-06-17 18:42:03 +0600

    Milestone one with sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
 import Wrapper from '../assets/wrappers/Navbar'
-import {FaHome} from 'react-icons/fa'
+import {FaAlignLeft} from 'react-icons/fa'
 
 import React from 'react'
+import Logo from './Logo'
+import { useDashboardConext } from '../pages/DashboardLayout'
 
 const Navbar = () => {
+   const {toggleSidebar} = useDashboardConext()
   return (
     <Wrapper>
-      <h1>Navbar</h1>
-      <FaHome />
+      <div className="nav-center">
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+            <FaAlignLeft />
+        </button>
+        <div>
+            <Logo/>
+            <h4 className='logo-text'>dashboard</h4>
+        </div>
+        <div className="btn-container">
+            toggle/logout
+        </div>
+      </div>
+      
     </Wrapper>
   )
 }
