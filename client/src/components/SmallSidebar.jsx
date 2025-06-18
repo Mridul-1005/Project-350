a9474f1f09ed51ac4be5b0a71c4460e4560697c1
Mridul-1005 <mhdanger193@gmail.com>
2025-06-18 10:46:22 +0600

    added sidebar functionalities

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -2,13 +2,41 @@ import Wrapper from '../assets/wrappers/SmallSidebar'
 
 import React from 'react'
 import { useDashboardConext } from '../pages/DashboardLayout'
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 const SmallSidebar = () => {
-  const data =  useDashboardConext();
-  console.log(data);
+  const {showSidebar, toggleSidebar} =  useDashboardConext();
+  
   return (
     <Wrapper>
-      <h1>Small Sidebar</h1>
+      <div className={showSidebar? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+        <div className="content">
+            <button type='button' className='close-btn' onClick={toggleSidebar}>
+                <FaTimes />
+
+            </button>
+            <header>
+                <Logo />
+            </header>
+            <div className="nav-links">
+                {links.map((link)=>{
+
+const {text, path, icon} = link
+return <NavLink to={path} key={text} className='nav-link' onClick={toggleSidebar} end>
+    <span className='icon'>
+{icon}
+    </span>
+    {text}
+</NavLink>
+                })}
+
+            </div>
+        </div>
+
+      </div>
     </Wrapper>
   )
 }
