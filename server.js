e6d76c2c47adff720dfbde306f13c31a032ab4bd
Mridul-1005 <mhdanger193@gmail.com>
2025-07-06 09:39:19 +0600

    adding backend

---
+++ server.js
@@ -0,0 +1,59 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+
+let jobs = [
+    {id: nanoid(), comapny: 'apple', position: 'front-end'},
+    {id: nanoid(), comapny: 'google', position: 'back-end'},
+
+]
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+   app.use(morgan('dev'));
+}
+
+
+
+
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+})
+
+app.post('/', (req, res)=>{
+    console.log(req);
+    res.json({message: 'Data received successfully', data: req.body});
+});
+
+//Get all jobs
+app.get('/api/v1/jobs', (req, res)=>{
+    res.status(200).json({jobs})
+});
+
+//Create a job
+app.post('/api/v1/jobs', (req, res)=>{
+    const {company, position } = req.body;
+    if(!company || !position){
+        return res.status(400).json({msg: 'Please provide company and position'});
+    }
+    const id = nanoid(10);
+    const job = {id, company, position};
+    jobs.push(job);
+    res.status(200).json({job})
+});
+
+const port = process.env.PORT || 5100;
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
