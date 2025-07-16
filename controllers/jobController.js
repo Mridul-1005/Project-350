8e86bfb9b520fb311687d8c8e524cdbf60f50372
Mridul-1005 <mhdanger193@gmail.com>
2025-07-17 04:09:10 +0600

    get all and single jobs

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -9,18 +9,20 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res)=>{
+    const jobs = await Job.find({});
     res.status(200).json({jobs});
 };
 
 export const createJob = async (req, res)=>{
   //  const {company, position } = req.body;
-    const job = await Job.create(req.body)
+    const job = await Job.create(req.body)  // 'something'
     res.status(201).json({job});  //modified code to 201 status code
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
+ 
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
