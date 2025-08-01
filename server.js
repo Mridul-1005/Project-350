a056869539f3ddc5293d465fad68ee1dcf2443e7
Mridul-1005 <mhdanger193@gmail.com>
2025-08-02 01:00:23 +0600

    added verification of cookie

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ import morgan from 'morgan';
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 // import { validateTest } from './middleware/validationMiddleware.js';
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import {authenticateUser} from './middleware/authMiddleware.js';
 
 
 
@@ -25,7 +27,7 @@ if(process.env.NODE_ENV === 'development'){
 
 
 
-
+app.use(cookieParser());
 app.use(express.json());
 
 
@@ -35,7 +37,7 @@ app.get('/', (req, res)=>{
 
 
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 
