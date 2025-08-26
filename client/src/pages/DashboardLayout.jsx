a9a2d11b704690214aa56a4109a9db33bb3df28e
Tafriya Amin Habiba <tafriya63746@gmail.com>
2025-08-26 21:06:42 +0600

    add dashboard loader

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,65 +1,64 @@
+import React, { useContext, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { createContext } from "react";
 
-
-import React, { useContext, useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import Wrapper from '../assets/wrappers/Dashboard'
-import { BigSidebar, Navbar, SmallSidebar } from '../components'
-import { createContext } from 'react'
-
-const DashboardContext = createContext();
-
-
-const DashboardLayout = ({isDarkThemeEnabled}) => {
-  //temp
-  const user = {name: 'Mridul'}
-  const [showSidebar,setShowSidebar] = useState(false);
-  const [isDarkTheme,setIsDarkTheme] = useState(isDarkThemeEnabled);
-
-const toggleDarkTheme = () =>{
-  const newDarkTheme = !isDarkTheme;
-  setIsDarkTheme(newDarkTheme);
-  document.body.classList.toggle('dark-theme', newDarkTheme);
-  localStorage.setItem('darkTheme', newDarkTheme);
+export const loader = () => {
   
 };
 
-const toggleSidebar = () =>{
-  setShowSidebar(!showSidebar);
-};
+const DashboardContext = createContext();
 
-const logoutUser = async ()=>{
-     console.log('logout user');
-};
+const DashboardLayout = ({ isDarkThemeEnabled }) => {
+  const data = useLoaderData();
+  console.log(data);
+  //temp
+  const user = { name: "Mridul" };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
 
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log("logout user");
+  };
 
   return (
-    <DashboardContext.Provider 
-    value={{
-      user,
-       showSidebar,
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
         isDarkTheme,
-         toggleDarkTheme,
-          toggleSidebar,
-           logoutUser
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
       }}
-      >
-    <Wrapper>
-      <main className="dashboard">
-        <SmallSidebar/>
-        <BigSidebar />
-        <div>
-          <Navbar/>
-          <div className='dashboard-page'>
-            <Outlet />
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
             </div>
-        </div>
-      </main>
-      
-    </Wrapper>
+          </div>
+        </main>
+      </Wrapper>
     </DashboardContext.Provider>
   );
 };
 
-export const useDashboardContext = ()=> useContext(DashboardContext);
+export const useDashboardContext = () => useContext(DashboardContext);
 export default DashboardLayout;
