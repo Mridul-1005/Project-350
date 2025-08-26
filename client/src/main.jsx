57a48428d522b604e3f9c7aaa21d39dfe95a819c
Tafriya Amin Habiba <tafriya63746@gmail.com>
2025-08-26 19:30:10 +0600

    feat: enhance registration flow with success and error notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./App.jsx";
+import 'react-toastify/dist/ReactToastify.css';
+import "./index.css";
+import{ToastConatiner} from 'react-toastify'
 
 //  fetch('/api/v1/test').then((res)=> res.json()).then((data)=>console.log(data));
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+       <ToastContainer position='top-center'/>
+  </StrictMode>
+);
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,20 +3,24 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 export const action = async (request) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   try {
     await customFetch.post("/auth/register", data);
-     return redirect('/login');
+    toast.success("Registration successful");
+    return redirect("/login");
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
- 
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -32,8 +36,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="earth" />
         <FormRow type="email" name="email" defaultValue="mridul@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Already have an account?{" "}
