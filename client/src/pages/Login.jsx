1e2ef0d367176bf0e75bcba1f376d260600f15b1
Tafriya Amin Habiba <tafriya63746@gmail.com>
2025-08-26 20:24:16 +0600

    add login action and integrate with login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,20 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link,Form,redirect,useNavigation } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+export const action = async({request})=>{
+  const formData =await request.formData()
+  const data = Object.fromEntries(formData)
+  try{
+await customFetch.post('/auth/login',data)
+toast.success("Login successful")
+return redirect('/dashboard')
+  }
+  catch(error){
+toast.error(erroe?.response?.data?.msg);
+return error;
+  }
+  
+};
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state ==="submitting";
   return (
     <Wrapper>
-        <form className='form'>
+        <Form method ='post'  className='form'>
             <Logo />
             <h4>Login</h4>
             <FormRow type="email" name="email" defaultValue="mridul@gmail.com" />
              <FormRow type="password" name="password" defaultValue="secret123" />
-             <button type='submit' className='btn btn-block'>Submit</button>
-             <button type='button' className='btn btn-block'>Explore the app</button>
-             <p>Don't have an account? <Link to='/register' className='member-btn'>Register</Link></p>
-        </form>
+             <button type='submit' className='btn btn-block' disabled={isSbumitting}>
+             {isSbumitting ? "submitting...":"submit"}
+             </button>
+             <button type='button' className='btn btn-block'>
+             Explore the app
+             </button>
+             <p>
+             Don't have an account? 
+             <Link to='/register' className='member-btn'>Register</Link></p>
+        </Form>
     </Wrapper>
   );
 }
