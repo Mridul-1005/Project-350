cfb1fae3025af99a95760d24f33a5602bab32158
Tafriya Amin Habiba <tafriya63746@gmail.com>
2025-09-01 09:53:46 +0600

    implement job addition form with user location

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { FormRow, FromRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, redirect, useNavigation } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
 
 const AddJob = () => {
-  return <h1>Add Job Page</h1>
-}
+  const {user}=useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state ==='submitting';
+  return <Wrapper>
+  <Form method="post" className="form">
+  <h4 className="form-title">Add Tution</h4>
+  <div className="form-center">
+  <FormRow type="text" name='position'/>
+  <FormRow type="text" name='company'/>
+  <FormRow type='text'  labelText='job location'  name='jobLocation'
+  defaultValue={user.location}/>
+  <button type='submit' className="btn btn-block form-btn"
+  disabled={isSubmitting}
+  >
+  {isSubmitting ? 'submitting': 'submit'}
+  </button>
+  </div>
+  </Form>
+  </Wrapper>
+};
 
-export default AddJob
+export default AddJob;
